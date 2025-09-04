3a3827bb538e18ac400acd3064c6c39d64f0e94b
Joyce Ng <joyce.ng@oocl.com>
2025-09-04 17:59:15 +0800

    refactor: simplify countFrequencies method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -33,32 +33,15 @@ public class WordFrequencyGame {
     }
 
     private List<Input> countFrequencies(String[] words) {
+        Map<String, Integer> wordCounts = new HashMap<>();
+        for (String word : words) {
+            wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+        }
+
         List<Input> frequencies = new ArrayList<>();
-        for (Map.Entry<String, List<String>> entry : groupSameWords(words).entrySet()) {
-            Input input = new Input(entry.getKey(), entry.getValue().size());
-            frequencies.add(input);
+        for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+            frequencies.add(new Input(entry.getKey(), entry.getValue()));
         }
         return frequencies;
-    }
-
-    private static Map<String, List<String>> groupSameWords(String[] words) {
-        List<String> inputList = new ArrayList<>();
-        for (String s : words) {
-            inputList.add(s);
-        }
-
-        //get the map for the next step of sizing the same word
-        Map<String, List<String>> map = new HashMap<>();
-        for (String input1 : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input1)) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input1);
-                map.put(input1, arr);
-            } else {
-                map.get(input1).add(input1);
-            }
-        }
-        return map;
     }
 }
